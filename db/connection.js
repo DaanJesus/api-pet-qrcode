fc1f569c1e62d79f20037d2a66518d9070ef7cce
DaanJesus <danilo25oliveira@gmail.com>
2024-09-27 18:40:33 -0300

    fix cors

--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,5 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@forsale.ukynm.mongodb.net/tagMyPet?retryWrites=true&w=majority&appName=ForSale`;
-console.log(process.env.DB_USERNAME, process.env.DB_PASSWORD);
 
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log('Connected to DB')).catch((e) => console.log('Error', e))
--- index.js
+++ index.js
@@ -9,13 +9,25 @@ require('./db/connection');
 
 const app = express();
 
+const allowedOrigins = ['https://tag-my-pet.vercel.app', 'http://localhost:4200']
+
 // Middleware
 app.use(bodyParser.json());
-app.use(cors({
-    origin: 'https://tag-my-pet.vercel.app',
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization'
-}));
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            // Permitir a origem se estiver na lista ou se for um request sem origem (como o cURL)
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 
 // Rotas
 app.get('/', (req, res) => {
