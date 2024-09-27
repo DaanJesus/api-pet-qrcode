f5f01ecdabde9eb59bc4f47e8e076440d734241d
DaanJesus <danilo25oliveira@gmail.com>
2024-09-27 14:47:40 -0300

    config cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'http://localhost:4200',
+    origin: 'https://tag-my-pet.vercel.app/',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization'
 }));
